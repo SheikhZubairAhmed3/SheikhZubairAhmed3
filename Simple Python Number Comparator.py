5b3d6cb71924f52fdb906f26877e3b1bfc8b81d3
SheikhZubairAhmed3 <105794849+SheikhZubairAhmed3@users.noreply.github.com>
2022-05-18 18:44:16 +0530

    Simple Python Number Comparator
    This is a code for a simple python number comparator.
    It basically compares numbers you put into it.
    it can compare two numbers.
    Step 1: Put the numbers in the a and b slots.
    Step 2 : Save the code and run it 
    More details on how the code works will be added soon.
    If you like my code please follow my account and also subscribe to my youtube channel -

---
+++ Simple Python Number Comparator.py
@@ -0,0 +1,25 @@
+#==============================================================================#
+#Made By : Zubair.
+#Greater Than,Less Than or Equal To Checker.
+#==============================================================================#
+#Put in the numbers in a and b then save and run.
+#==============================================================================#
+
+a = 12
+b = 5756
+
+def myfunc():
+   global b
+   global b
+
+if a > b:
+  print("a is greater than b")
+elif a==b:
+  print("a is equal to b")
+else:
+  print("b is greater than a")
+
+myfunc()
+
+
+
